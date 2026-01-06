9623deaf84f099a06a903768ea55afa74b497e3f
chihling830 <162959755+chihling830@users.noreply.github.com>
2026-01-06 18:29:19 +0800

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-載入 model.pkl + 測試資料，輸出報告 & 混淆矩陣
-用法：
+載入 model.pkl + 測試資料
 python src/evaluate.py --model output/model.pkl --test data/processed/features.csv --report output/report.txt
 """
 import argparse, joblib, pandas as pd, matplotlib.pyplot as plt
@@ -14,7 +13,6 @@ def evaluate(model_path: str, test_csv: str, report_path: str) -> None:
     X_test, y_test = df.drop('label', axis=1), df['label']
     y_pred = model.predict(X_test)
 
-    # 文字報告
     report_txt  = classification_report(y_test, y_pred)
     cm          = confusion_matrix(y_test, y_pred)
     with open(report_path, "w") as f:
@@ -24,7 +22,7 @@ def evaluate(model_path: str, test_csv: str, report_path: str) -> None:
         f.write(cm.__repr__())
     print(" Saved report to", report_path)
 
-    # 圖形混淆矩陣
+    # 混淆矩陣
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['normal','abnormal'])
     disp.plot(cmap=plt.cm.YlOrRd, values_format='g')
     plt.show()
